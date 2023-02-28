bf6dd11485f0356f662e6e470902d388115c31b2
abhishekpyasi <pyasi.abhishek@gmail.com>
2023-02-28 16:13:33 +0530

    First Commit after Inventory service

---
+++ Play.Inventory/src/Play.Inventory.Service/Dtos.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Play.Inventory.Service
+{
+    public record GrantItemsDto(Guid UserId, Guid CatalogItemID, int Quantity);
+
+    public record InventoryItemDto(Guid CatalogItemId, int Quantity, DateTimeOffset AcquireDate);
+
+    public record CatalogItemDto(Guid Id, string Name, string Description);
+
+
+
+
+}
