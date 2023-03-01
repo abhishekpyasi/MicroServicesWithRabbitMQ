12932b7362a3f82521629a40a0c28836c73a9e95
abhishekpyasi <pyasi.abhishek@gmail.com>
2023-03-01 17:52:38 +0530

    After setting up RabbitMQ

---
+++ Play.Catlog/src/Play.Catlog.Service/Settings/RabbitMQSettings.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Play.Catlog.Service.Settings
+{
+    public class RabbitMQSettings
+    {
+        public string Host { get; init; }
+    }
+}
